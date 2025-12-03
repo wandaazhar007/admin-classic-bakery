fd93a8dc9aaf464e766b94fd2c9997b2c69e3c1e
wanda azhar <wandaazhar@gmail.com>
2025-12-03 02:06:03 -0700

    install fontAwesomeIcon

---
+++ src/lib/fontawesome.ts
@@ -0,0 +1,5 @@
+// src/lib/fontawesome.ts
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+// Prevent Font Awesome from adding its CSS automatically
+config.autoAddCss = false;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import "./lib/fontawesome";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
