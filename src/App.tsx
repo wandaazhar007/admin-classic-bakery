f076422552d9b11d1433042232afca47ad76cf3b
wanda azhar <wandaazhar@gmail.com>
2025-12-17 01:46:22 -0700

    update layouting

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 // src/App.tsx
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Sidebar from "./components/sidebar/Sidebar";
+// import Navbar from "./components/navbar/Navbar";
+// import Footer from "./components/footer/Footer";
+// import Sidebar from "./components/sidebar/Sidebar";
 import "../styles/_globals.scss";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
-  const closeSidebar = () => setIsSidebarOpen(false);
+  // const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
+  // const closeSidebar = () => setIsSidebarOpen(false);
 
   return (
     <div className="app-layout">
-      <Navbar onToggleSidebar={toggleSidebar} />
+      {/* <Navbar onToggleSidebar={toggleSidebar} /> */}
 
       <div className="app-shell">
-        <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+        {/* <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} /> */}
 
         <main className="app-content" id="main-content">
           <Outlet />
         </main>
       </div>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import "./lib/fontawesome";
 import "../styles/_globals.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
